fe109f64ad889dc0fd5a74dabe753adf294d228e
Thao Nguyen <thao.nguyen@readytech.io>
2024-11-05 16:06:07 +0700

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace workshop_dotnet.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class HomeController : ControllerBase
+    {
+        public HomeController()
+        {
+
+        }
+
+        [HttpGet("add")]
+        public IActionResult Add(int value)
+        {
+            var result = value + 1;
+            return Ok(result);
+        }
+
+        [HttpGet("minus")]
+        public IActionResult Minus(int value)
+        {
+            var result = value - 1;
+            return Ok(result);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+// Configure CORS to allow any origin
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+        builder => builder.AllowAnyOrigin()
+                          .AllowAnyHeader()
+                          .AllowAnyMethod());
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+// Use CORS policy
+app.UseCors("AllowAllOrigins");
+
+app.MapControllers();
+
+app.Run();
