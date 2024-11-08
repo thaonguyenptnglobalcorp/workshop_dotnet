c2cfff1f77978b0d104fe0a265affc09875425b7
Thao Nguyen <thao.nguyen@readytech.io>
2024-11-08 16:39:47 +0700

    update

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -14,7 +14,7 @@ namespace workshop_dotnet.Controllers
         [HttpGet("add")]
         public IActionResult Add(int value)
         {
-            var result = value + 1;
+            var result = value + 2;
             return Ok(result);
         }
 
