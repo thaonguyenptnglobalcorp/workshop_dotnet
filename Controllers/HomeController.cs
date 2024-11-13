711dc952221592a9e9e907d226e843a8b3cda335
Thao Nguyen <thaonm@cymosoft.com>
2024-11-13 23:00:24 +0700

    update api

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -14,8 +14,8 @@ namespace workshop_dotnet.Controllers
         [HttpGet("add")]
         public IActionResult Add(int value)
         {
-            // add
-            var result = value + 1;
+            // add by 2
+            var result = value + 2;
             return Ok(result);
         }
 
